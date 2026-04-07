5b5249acd07f499b87201ca3de5ba9012ab57e73
rajeshkanna6890 <s.rajeshkanna@kuwy.in>
2026-04-07 12:45:57 +0530

    error fixs

--- src/components/ui/rich-text-editor.tsx
+++ src/components/ui/rich-text-editor.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import ReactQuill from 'react-quill';
+import "react-quill/dist/quill.snow.css";
 import { cn } from '@/lib/utils';
 
 interface RichTextEditorProps {
